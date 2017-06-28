cc2b0ce8acbf3a22774e5be8bfd094f62126d441
hnkboy <709256704@qq.com>
2017-06-28 09:30:51 +0800

    Update hello.c

--- modules/test/hello.c
+++ modules/test/hello.c
@@ -2,15 +2,12 @@
 #include <linux/kernel.h>  
 #include <linux/module.h>  
    
-static int __init test_init(void)  
-{  
-       printk("init module/n");  
-  
-       return 0;  
+static int __init test_init(void) {  
+    printk("init module/n");  
+    return 0;  
 }  
-static void __exit test_exit(void)  
-{  
-       printk("exit modules/n");  
+static void __exit test_exit(void) {  
+    printk("exit modules/n");  
 }  
    
 module_init(test_init);  
