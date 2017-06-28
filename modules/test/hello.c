f65d21622255ba04ee57e674fcf2b02ee59560db
hnkboy <709256704@qq.com>
2017-06-28 13:33:33 +0800

    update

--- modules/test/hello.c
+++ modules/test/hello.c
@@ -1,7 +1,7 @@
 #include <linux/init.h>  
 #include <linux/kernel.h>  
 #include <linux/module.h>  
-   
+MODULE_LICENSE("Dual BSD/GPL");   
 static int __init test_init(void) {  
     printk("init module/n");  
     return 0;  
