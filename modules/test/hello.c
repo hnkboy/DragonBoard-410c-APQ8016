7f25cd909e6d3284f592356e188030206b6650d3
hnkboy <709256704@qq.com>
2017-06-28 08:36:34 +0800

    Create hello.c

---
+++ modules/test/hello.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>  
+#include <linux/kernel.h>  
+#include <linux/module.h>  
+   
+static int __init test_init(void)  
+{  
+       printk("init module/n");  
+  
+       return 0;  
+}  
+static void __exit test_exit(void)  
+{  
+       printk("exit modules/n");  
+}  
+   
+module_init(test_init);  
+module_exit(test_exit);  
